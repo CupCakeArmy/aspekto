799d74e7d03493c21729086c3df9dbbf5ba2bdcd
nicco <nicco.borgioli@gmail.com>
2019-02-06 01:08:38 +0100

    pass props to card

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
+import { removeRepeatingSpaces } from '../Util'
 
 export type CardProps = {}
 
-const Card: React.FC<CardProps & React.HTMLAttributes<HTMLElement>> = (props) => {
-	return <div className={'card'}>
+const Card: React.FC<CardProps & React.HTMLAttributes<HTMLElement>> = ({className, ...props}) => {
+	return <div className={removeRepeatingSpaces(`card ${className}`)} {...props}>
 		{props.children}
 	</div>
 }
