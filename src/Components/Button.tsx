04546d24771839726ec1a7102bf2fd4d1e17ab00
cupcakearmy <nicco.borgioli@gmail.com>
2019-02-09 22:38:05 +0100

    button tweaks

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react'
 import { removeRepeatingSpaces } from '../Util'
 
+type handleFN = React.TouchEvent | React.MouseEvent
 export type ButtonProps = {
-	handle?: (e: React.MouseEvent) => any
+	handle?: (e: handleFN) => any
 }
 
-const Button: React.FC<ButtonProps & React.HTMLAttributes<HTMLElement>> = ({ className, handle, children, ...props }) => {
-	const _click = (e: React.MouseEvent) => {
+const Button: React.FC<ButtonProps & React.HTMLAttributes<HTMLElement>> = ({ className, handle, children }) => {
+	const _click = (e: handleFN) => {
 		if (typeof handle === 'function') handle(e)
 	}
 
-	return <div className={removeRepeatingSpaces(`aspekto button ${className || ''}`)} onClick={_click}>
+	return <div onClick={_click} onTouchStart={_click}
+				className={removeRepeatingSpaces(`aspekto button ${className || ''}`)}>
 		<div className="text">{children}</div>
 		<div className="bg"/>
 	</div>
