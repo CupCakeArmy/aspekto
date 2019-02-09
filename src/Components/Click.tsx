bc3a90476118e3f72fbe6cd2fbe72495fb67af4f
cupcakearmy <nicco.borgioli@gmail.com>
2019-02-09 22:48:49 +0100

    Click mouse pointer

--- src/Components/Click.tsx
+++ src/Components/Click.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react'
+import { removeRepeatingSpaces } from '../Util'
 
 export type ButtonProps = {
 	handle?: () => any
 }
 
-export const Click: React.FC<ButtonProps & React.HTMLAttributes<HTMLElement>> = ({ handle, ...props }) => {
+export const Click: React.FC<ButtonProps & React.HTMLAttributes<HTMLElement>> = ({ handle, className, ...props }) => {
 	const _click = () => {
 		if (typeof handle === 'function') handle()
 	}
 
-	return <div {...props} onClick={_click} onTouchStart={_click}/>
+	return <div {...props} onClick={_click} onTouchStart={_click}
+				className={removeRepeatingSpaces(`aspekto click ${className || ''}`)}/>
 }
 
 export default Click
