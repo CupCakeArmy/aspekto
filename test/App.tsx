084f0cc1015752fba87e463e13c671d32fdd78a6
cupcakearmy <nicco.borgioli@gmail.com>
2019-02-09 16:07:08 +0100

    added HR

--- test/App.tsx
+++ test/App.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 
-import { Button, Card, Flex, Input, Switch } from '../lib/index'
+import { Button, Card, Flex, Input, Switch, HR } from '../lib/index'
 
 const App: React.FC = () => {
 	return <div className={'fill'} style={{ fontFamily: `'Helvetica Neue', sans-serif` }}>
 		<Flex.Container center className={'fill'}>
 			<Flex.Item shrink>
 				<Card>
+					<h1>Form</h1>
 					<Input label={'username'}/>
 					<Input label={'password'} type={'password'}/>
 					<br/>
@@ -16,6 +17,8 @@ const App: React.FC = () => {
 					<Button handle={() => console.log('Test')}>
 						login
 					</Button>
+					<HR vertical/>
+					<h2>Sub</h2>
 				</Card>
 			</Flex.Item>
 		</Flex.Container>
