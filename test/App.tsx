e20a936c8129ebfd891fc1a67f15691aaacb9497
cupcakearmy <nicco.borgioli@gmail.com>
2019-02-16 19:57:05 +0100

    form helper example

--- test/App.tsx
+++ test/App.tsx
@@ -1,26 +1,31 @@
 import * as React from 'react'
 
-import { Button, Card, Flex, Input, Switch, HR, Click } from '../lib/index'
-
+import { Button, Card, Click, Flex, HR, Switch, useFormHelper, Input } from '../lib/index'
 
 const App: React.FC = () => {
+	// const [username, setUsername] = useState({})
+	const [handler, submit] = useFormHelper()
+
 	return <div className={'fill'} style={{ fontFamily: `'Helvetica Neue', sans-serif` }}>
 		<Flex.Container center className={'fill'}>
 			<Flex.Item shrink>
 				<Card>
 					<h1>Form</h1>
-					<Input label={'username'}/>
-					<Input label={'password'} type={'password'}/>
+					<Input label={'username'} valid={s => /test/.test(s)} handle={handler()} disabled/>
+					<Input label={'password'} type={'password'} handle={handler()}/>
+					<Input label={'repeat-password'} type={'password'} handle={handler()}/>
 					<br/>
 					Keep Session <Switch checked={true} handle={console.log}/>
 					<br/>
 					<br/>
-					<Button handle={() => console.log('Test')}>
+					<Button handle={() => {
+						console.log(submit())
+					}}>
 						login
 					</Button>
 					<HR horizontal/>
 					<h2>Sub</h2>
-					<Click handle={()=>console.log('test')}><i className="icon ion-md-heart"/></Click>
+					<Click handle={() => console.log('test')}><i className="icon ion-md-heart"/></Click>
 				</Card>
 			</Flex.Item>
 		</Flex.Container>
